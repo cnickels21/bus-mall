1dbf52f90f833181185a543acd255103f983107e
cnickels21 <cnickels_21@yahoo.com>
2020-03-09 15:59:57 -0500

    js event function nearly finished

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var imageElements =  document.getElementsByTagName('img');
+var imageElements =  document.getElementsByClassName('clickers');
 var imageIndexOne = 0;
 var imageIndexTwo = 1;
 var imageIndexThree = 2;
+var rounds = 25;
 var totalClicks = 0;
 var allImages = [];
 
@@ -11,6 +12,7 @@ function Image(name, imageUrl) {
     this.name =  name;
     this.imageUrl = imageUrl;
     this.timesClicked = 0;
+    this.timesShown = 0;
     allImages.push(this);
 }
 
@@ -39,12 +41,47 @@ function clickedImage(event) {
     totalClicks++;
     if(event.srcElement.id === 'first-image') {
         allImages[imageIndexOne].timesClicked++;
+        allImages[imageIndexOne].timesShown++;
     } else if (event.srcElement.id === 'second-image') {
         allImages[imageIndexTwo].timesClicked++;
+        allImages[imageIndexTwo].timesShown++;
+    } else if (event.srcElement.id === 'third-image') {
+        allImages[imageIndexThree].timesClicked++;
+        allImages[imageIndexThree].timesShown++;
     }
-    if(imageIndexOne + imageIndexTwo >= 25) {
-        var footerElement = document.getElementsByTagName('footer')[0];
-        footerElement.textContent = `You picked the first image ${imageIndexOne} times and the second image ${imageIndexTwo} times.`;
+    var nextFirstImage = Math.floor(Math.random() * allImages.length);
+    while ((nextFirstImage === imageIndexOne) || (nextFirstImage === imageIndexTwo) || (nextFirstImage === imageIndexThree) || (nextSecondImage === nextFirstImage) || (nextThirdImage === nextFirstImage)) {
+        nextFirstImage = Math.floor(Math.random() * allImages.length);
+    }
+
+    var nextSecondImage = Math.floor(Math.random() * allImages.length);
+    while ((nextSecondImage === imageIndexTwo) || (nextSecondImage === imageIndexOne) || (nextSecondImage === imageIndexThree) || (nextSecondImage === nextFirstImage) || (nextSecondImage === nextThirdImage)) {
+        nextSecondImage = Math.floor(Math.random() * allImages.length);
+    }
+
+    var nextThirdImage = Math.floor(Math.random() * allImages.length);
+    while ((nextThirdImage === imageIndexThree) || (nextThirdImage === imageIndexOne) || (nextThirdImage === imageIndexTwo) || (nextThirdImage === nextFirstImage) || (nextThirdImage === nextSecondImage)) {
+        nextThirdImage = Math.floor(Math.random() * allImages.length);
+    }
+
+    imageIndexOne = nextFirstImage;
+    imageIndexTwo = nextSecondImage;
+    imageIndexThree = nextThirdImage;
+
+    imageElements[0].src = allImages[imageIndexOne].imageUrl;
+    imageElements[1].src = allImages[imageIndexTwo].imageUrl;
+    imageElements[2].src = allImages[imageIndexThree].imageUrl;
+
+    console.log(imageIndexOne);
+
+    if(totalClicks >= rounds) {
+
+        var asideElement = document.getElementsByTagName('aside')[0];
+        asideElement.textContent = ``;
+
+        for (var j = 0; j < imageElements.length; j++) {
+            imageElements[j].removeEventListener('click', clickedImage);
+        }
     }
 }
 
