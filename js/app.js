12170d7b5b997833edb02482e3af488c5405d45b
cnickels21 <cnickels_21@yahoo.com>
2020-03-09 14:39:38 -0500

    images added and html\css started

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// var firstImageClicked = 0;
-// var secondImageClicked = 0;
 var imageElements =  document.getElementsByTagName('img');
 var imageIndexOne = 0;
 var imageIndexTwo = 1;
+var imageIndexThree = 2;
 var totalClicks = 0;
 var allImages = [];
 
@@ -15,10 +14,26 @@ function Image(name, imageUrl) {
     allImages.push(this);
 }
 
-// new Image('', '')
-// new Image('', '')
-// new Image('', '')
-// new Image('', '')
+new Image('Bag', 'images/bag.jpg');
+new Image('Banana', 'images/banana.jpg');
+new Image('Bathroom', 'images/bathroom.jpg');
+new Image('Boots', 'images/boots.jpg');
+new Image('Breakfast', 'images/breakfast.jpg');
+new Image('Bubblegum', 'images/bubblegum.jpg');
+new Image('Chair', 'images/chair.jpg');
+new Image('Cthulhu', 'images/cthulhu.jpg');
+new Image('Dog-Duck', 'images/dog-duck.jpg');
+new Image('Dragon', 'images/dragon.jpg');
+new Image('Pen', 'images/pen.jpg');
+new Image('Pet-Sweep', 'images/pet-sweep.jpg');
+new Image('Scissors', 'images/scissors.jpg');
+new Image('Shark', 'images/shark.jpg');
+new Image('Sweep', 'images/sweep.png');
+new Image('Tauntaun', 'images/tauntaun.jpg');
+new Image('Unicorn', 'images/unicorn.jpg');
+new Image('Usb', 'images/usb.gif');
+new Image('Water-Can', 'images/water-can.jpg');
+new Image('Wine-Glass', 'images/wine-glass.jpg');
 
 function clickedImage(event) {
     totalClicks++;
