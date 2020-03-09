75b32538fb8764dfc32a2d5c649cea3e0620ccec
cnickels21 <cnickels_21@yahoo.com>
2020-03-09 13:38:02 -0500

    minor changes before renaming my branch

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 // var firstImageClicked = 0;
 // var secondImageClicked = 0;
+var imageElements =  document.getElementsByTagName('img');
 var imageIndexOne = 0;
 var imageIndexTwo = 1;
 var totalClicks = 0;
@@ -10,26 +11,28 @@ var allImages = [];
 function Image(name, imageUrl) {
     this.name =  name;
     this.imageUrl = imageUrl;
+    this.timesClicked = 0;
+    allImages.push(this);
 }
 
-new Image('', '')
-new Image('', '')
-new Image('', '')
-new Image('', '')
+// new Image('', '')
+// new Image('', '')
+// new Image('', '')
+// new Image('', '')
 
-function clickedImage() {
+function clickedImage(event) {
+    totalClicks++;
     if(event.srcElement.id === 'first-image') {
-        firstImageClicked++;
+        allImages[imageIndexOne].timesClicked++;
     } else if (event.srcElement.id === 'second-image') {
-        secondImageClicked++;
+        allImages[imageIndexTwo].timesClicked++;
     }
-    if(firstImageClicked + secondImageClicked >= 5) {
+    if(imageIndexOne + imageIndexTwo >= 25) {
         var footerElement = document.getElementsByTagName('footer')[0];
-        footerElement.textContent = `You picked the first image ${firstImageClicked} times and the second image ${secondImageClicked} times.`;
+        footerElement.textContent = `You picked the first image ${imageIndexOne} times and the second image ${imageIndexTwo} times.`;
     }
 }
 
-var imageElements =  document.getElementsByTagName('img');
 for (var i = 0; i < imageElements.length; i++) {
     imageElements[i].addEventListener('click', clickedImage);
 }
