b0dec69b05cc55cef0fe1aafc015322f42cccf0c
cnickels21 <cnickels_21@yahoo.com>
2020-03-10 12:06:05 -0500

    table rendered but not customized

--- js/app.js
+++ js/app.js
@@ -16,6 +16,13 @@ function Image(name, imageUrl) {
     allImages.push(this);
 }
 
+function getAllImages(propertyAnalyzed) {
+    var answer = [];
+    for (var k = 0; k < allImages.length; k++) {
+        
+    }
+}
+
 new Image('R2D2 Bag', 'images/bag.jpg');
 new Image('Banana Slicer', 'images/banana.jpg');
 new Image('Mud Boots', 'images/boots.jpg');
@@ -89,7 +96,48 @@ function clickedImage(event) {
             var listItems = document.createElement('li');
             listItems.textContent = (allImages[x].name + ' had ' + allImages[x].timesClicked + ' votes and was shown ' + allImages[x].timesShown + ' times');
             unorderedList.appendChild(listItems);
-            
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ,             //add function
+                    datasets: [{
+                        label: '# of Votes',
+                        data: ,            //add function
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+
+            // var percentageLi = document.createElement('li');
+            // percentageLi.textContent = (allImages[i].name + ' had a ');
+            // unorderdList.appendChild(percentageLi);
         }
     }
 }
